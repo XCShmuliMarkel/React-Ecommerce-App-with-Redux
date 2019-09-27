10f30fbdf1952b7cefb498aaba19d74cd255b9ff
JD <jdority@salesforce.com>
2019-09-26 20:50:58 -0700

    test

--- src/auth.js
+++ src/auth.js
@@ -14,3 +14,11 @@ conn.login('admin@rcg99.demo', 'Salesforce1' + 'u2HY0jp6NGPmivFuxSqxaNf2', funct
     }
     console.log('User ID: ' + userInfo.id);
 });
+
+var records = [];
+
+conn.query("SELECT Id, Name FROM Account", function(err, result) {
+  if (err) { return console.error(err); }
+  console.log("total : " + result.totalSize);
+  console.log("fetched : " + result.records.length);
+});
