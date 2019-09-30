01e44859650053326454d6d7f0b2d3120ae8c3f3
JD <jdority@salesforce.com>
2019-09-30 13:31:11 -0700

    Use Product List

--- src/data/phones.js
+++ src/data/phones.js
@@ -1,4 +1,5 @@
 import Records from "../auth";
+
 console.log("Phones.js:", Records);
 
 export const phones = [{
@@ -6,14 +7,14 @@ export const phones = [{
     "category": "phone",
     "images": ["https://productimages.hepsiburada.net/s/18/280-413/9801258663986.jpg?v1", "https://productimages.hepsiburada.net/s/18/280-413/9801258696754.jpg?v1", "https://productimages.hepsiburada.net/s/18/280-413/9801258729522.jpg?v1", "https://productimages.hepsiburada.net/s/18/280-413/9801258762290.jpg?v1"],
     "brand": "apple",
-    "price": 4241.499828399639,
+    "price": Records[0],
     "cpu": "1.3GHz Apple A6",
     "camera": "8mp (3264x2448)",
     "size": "124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)",
     "weight": "132 grams (4.7 ounces) with battery",
     "display": "4.0 326 pixel density",
     "battery": "1480 mAh",
-    "memory": "16GB, 32GB and RAM 1 GB",
+    "memory": "16GB, 32GB and RAM 1 GB", 
     "id": 0,
     "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
 }, {
