e421bab376c2aa01698afbbfea6fe911e329b081
JD <jdority@salesforce.com>
2019-10-01 05:06:55 -0700

    for loop

--- src/data/phones.js
+++ src/data/phones.js
@@ -3,33 +3,21 @@ import Records from "../auth";
 console.log("Phones.js:", Records);
 
 export const phones = [{
-    "title": "Apple iPhone 7 Plus 32 GB (Apple Türkiye Garantili)",
-    "category": "phone",
-    "images": ["https://productimages.hepsiburada.net/s/18/280-413/9801258663986.jpg?v1", "https://productimages.hepsiburada.net/s/18/280-413/9801258696754.jpg?v1", "https://productimages.hepsiburada.net/s/18/280-413/9801258729522.jpg?v1", "https://productimages.hepsiburada.net/s/18/280-413/9801258762290.jpg?v1"],
-    "brand": "apple",
-    "price": Records[0],
-    "cpu": "1.3GHz Apple A6",
-    "camera": "8mp (3264x2448)",
-    "size": "124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)",
-    "weight": "132 grams (4.7 ounces) with battery",
-    "display": "4.0 326 pixel density",
-    "battery": "1480 mAh",
-    "memory": "16GB, 32GB and RAM 1 GB", 
-    "id": 0,
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
-}, {
-    "title": "Huawei Mate 20 Lite 64 GB (Huawei Türkiye Garantili)",
-    "category": "phone",
-    "images": ["https://productimages.hepsiburada.net/s/21/280-413/9933217792050.jpg?v1", "https://productimages.hepsiburada.net/s/21/280-413/9933217628210.jpg?v1", "https://productimages.hepsiburada.net/s/21/280-413/9933217660978.jpg?v1", "https://productimages.hepsiburada.net/s/21/280-413/9933217693746.jpg?v1"],
-    "brand": "huawei",
-    "price": 1823.6625483451157,
-    "cpu": "1.3GHz Apple A6",
-    "camera": "8mp (3264x2448)",
-    "size": "124.4mm x 59.2mm x 8.97mm (4.9 x 2.33 x 0.35)",
-    "weight": "132 grams (4.7 ounces) with battery",
-    "display": "4.0 326 pixel density",
-    "battery": "1480 mAh",
-    "memory": "16GB, 32GB and RAM 1 GB",
-    "id": 1,
-    "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!"
-}]
+    "title": "",
+    "category": "",
+    "images": "",
+    "price": 9,
+    "cpu": "",
+    "camera": "",
+    "size": "",
+    "weight": "",
+    "display": "",
+    "battery": "",
+    "memory": "",
+    "id": "",
+    "description": ""
+}];
+
+for (var i=0; i<1; i++)
+{phones[i].id = i;}
+console.log("Phones.js:", phones);
